1a2870045bc2c67207134fd1148fee51ec46a056
Gianfranco <40763595+giandb99@users.noreply.github.com>
2018-09-10 12:23:41 -0300

    tarea xd

---
+++ tarea/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    return 0;
+}
+/*
+
+declarar 3 variables tipo cadena (char)
+primeras 2 variables nombre y apellido (ç20 caracteres cada una)
+la tercer variable se muestra apellido y nombre y despues mostrar;
+
+(nombre Juan Carlos)
+(apellido Gomez)
+(apellidoynombre Gomez Juan Carlos)
+
+*/
