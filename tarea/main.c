a0f44fccefb13131527c00b5e1bbde37ea9859a9
Gianfranco <40763595+giandb99@users.noreply.github.com>
2018-09-10 23:21:58 -0300

    tarea "hecha"

--- tarea/main.c
+++ tarea/main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+    char nombre[20];
+    char apellido[20];
+    char apellidoYNombre;
+
+    printf("Ingrese el nombre: ");
+    gets(nombre);
+
+    printf("\nIngrese el apellido: ");
+    gets(apellido);
+
+    printf("\nApellido y nombre: %s %s",apellido,nombre);
+    printf("\n");
 
     return 0;
 }
 /*
 
 declarar 3 variables tipo cadena (char)
-primeras 2 variables nombre y apellido (ç20 caracteres cada una)
+primeras 2 variables nombre y apellido (20 caracteres cada una)
 la tercer variable se muestra apellido y nombre y despues mostrar;
 
 (nombre Juan Carlos)
